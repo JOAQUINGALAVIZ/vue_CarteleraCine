8f4cd076d90c6da9d263ad6b10f64556ccbdf357
Gustavo Herrera <guss_herrera_r@hotmail.com>
2023-12-02 03:21:49 -0600

    Se agrega vue router y las vistas de inicio y favoritos, y en el header el logo de la pagina y la navbar

--- Cartelera_vue/src/App.vue
+++ Cartelera_vue/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-import Header from './components/Header.vue';
+  import { RouterView } from 'vue-router'
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-    <Header />
-
+  <Header />
+  <main class="container mx-auto mt-10">
+    <RouterView />
+  </main>
 </template>
 
---
+++ Cartelera_vue/src/views/InicioView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-6xl font-extrabold">Péliculas</h1>
+</template>
